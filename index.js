a31fe9757ae2495315a059830b57bfd49b310f58
EastonMcCollum <91627611+EastonMcCollum@users.noreply.github.com>
2021-11-08 16:34:54 -0500

    working on task 5

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  counter one will retain the last number and add onto it, counter 2 just adds one to 0
   2. Which of the two uses a closure? How can you tell?
-  
+  counter one, it has multiple functions 
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter one is good for keeping track of a score maybe, counter 2 would probably be better for a que of some sorts.
 */
 
 // counter1 code
@@ -62,9 +63,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    const randNum = Math.floor(Math.random() * 3)
+    return randNum
 }
+//console.log(inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,19 +84,31 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningcb, num){
+  const score = {
+    Home: inningcb(),
+    Away: inningcb(),
+  }
+  for(let i = 0;i < num; i++){
+    i++
+  }
+  return score
 }
+//console.log(finalScore(inning, 9))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  const score = {
+    Home:inningcb(),
+    Away:inningcb()
+  }
+  return score
 }
-
+console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
